a7cc4cb6478d6a3b0a0a2761692da2cd8827e163
mariavteixeira <96mariavteixeira@users.noreply.github.com>
2020-12-12 22:37:51 -0300

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -20,16 +20,21 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+    
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "5e8b15f8fae68ac9a1de92e8db8a9b7b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Rio de Janeiro&appid=${apiKey}&units=metric`
+let city = "Rio de Janeiro"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
 
 axios.get(apiUrl).then(displayTemperature);
